99e3e4d40c15097d8b2078ebe00abaaea2e58142
Emily Anne Park <epark3@nd.edu>
2017-03-24 18:25:21 -0400

    updated 03/24

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Operating-Systems-Project-4
 Producer/Consumer design using Locks and Condition Variables to monitor a set of webpages
+
+# Members
+Luis Prieb (lprieb), Emily Park (epark3)
+
+# Description
+
+This program uses a series of threads to  monitor a list of sites and check whether each site contains any of a list of search terms. The program takes a single parameter, which is a configuration file. The configuration takes a parameter per line in the format PARAMETER=VALUE. If a parameter is not provided, default values are assumed. Even if no parameter is changed, an empty config file should be provided. Below are all the accepted parameter names with their meanings and default values:
+
+Parameter	Description							Default
+PERIOD_FETCH	The time (in seconds) between fetches of the various sites	180
+NUM_FETCH	Number of fetch threads (0 to 8)				1
+NUM_PARSE	Number of parsing threads (0 to 8)				1
+SEARCH_FILE	File containing the search strings				Search.txt
+SITE_FILE	File containing the sites to query				Sites.txt
+
+
+Internally the program works as follows. There are 4 types of threads: a timer thread, fetch threads, parser threads, and a result printer thread. The timer thread puts the sites in a queue that the fetch threads pop from to parse a site. It will do this every period of the time provided in the config file. The fetch threads use the libcurl library to get data from the current website in question and put the data into another queue that will be processed later. The parser threads read from the data queue and check for all instances of the search terms. The results are put into one final queue for results. The number of parser threads and fetch threads should be specified in the configuration file. The last thread waits for all the processing to finish using a global variable that all threads count up in. When all threads are done, a global result variable is written into a file. 
+
+# Installing
+
+A make file is provided for simple installion. Simply run
+
+$ make
+
+# Running
+
+To run the program simply run the program with the config file. For example, if the config file is config.txt and the executable is called project4, run the program as follows
+
+$ ./project4 config.txt
+
+To close the program, send the SIGINT signal using Ctrl+C.
+
+# Output
+
+Each time a result is ready, the result is written into a file called #.csv, where # is the current iteration of the program. Each line in the file will be formatted as follows:
+
+Time/Date,SearchTerm,Site,WordCount
+
+
+# Possible Errors
+
+If a period that is too short for the process to finish is given, undefined behavior occurs.
+
+
--- project4.cpp
+++ project4.cpp
@@ -11,20 +11,9 @@
 #include <pthread.h>
 #include <curl/curl.h>
 #include <ctime>
+#include <csignal>
 
 using namespace std;
-
-// Prototypes
-void parse_config(string fileName, params &paramO);
-bool test_digit(string word);
-void* timer_func(void*);
-void* threadFetch(void*);
-void* threadParse(void*);
-string our_curl(string website);
-static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
-string get_time(void);
-void* parseResults(void*);
-
 
 // Structs
 struct MemoryStruct {
@@ -32,10 +21,30 @@ struct MemoryStruct {
   size_t size;
 };
 
+typedef struct our_string {
+	string data;
+	bool success;
+} our_string;
+
 struct queueDObject{
 	string data;
 	string URL;
 };
+
+// Prototypes
+void parse_config(string fileName, params &paramO);
+bool test_digit(string word);
+void* timer_func(void*);
+void* threadFetch(void*);
+void* threadParse(void*);
+our_string our_curl(string website);
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
+string get_time(void);
+void* parseResults(void*);
+string form_time(struct tm*);
+void quit_handler(int s);
+void alarmHandler(int sig);
+
 
 
 // Global Variables
@@ -44,17 +53,19 @@ queue<queueDObject> dataQ;
 pthread_mutex_t sitesQLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t dataQLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t resultsLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t alarmLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t sitesQCond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t dataQCond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t resultsCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t alarmCond = PTHREAD_COND_INITIALIZER;
 string results;
 int nDone;
 bool Running;
+params paramO;
 
 int main(int argc, char *argv[])
 {
 	string configFN;// Configuration File name
-	params paramO; // Params objects
 	Running = true;
 	if(argc == 2)
 	{
@@ -63,70 +74,131 @@ int main(int argc, char *argv[])
 	else
 	{
 		cout << "Usage: " << argv[0] << " configuration_file_name" << endl;
+		exit(1);
 	}
 	
-	parse_config(configFN, paramO);
-	
+	parse_config(configFN, paramO);	
+
+	// Set signal to catch ctrl+c
+	signal(SIGINT, quit_handler);
+
 	// Initialize curl
-	curl_global_init(CURL_GLOBAL_ALL);
-	
+	curl_global_init(CURL_GLOBAL_ALL);	
 
 	// Launch Timer
 	pthread_t timer_t;
-	pthread_create(&timer_t,NULL, timer_func, &paramO);
-
+	pthread_create(&timer_t, NULL, timer_func, &paramO);
 
 	// Launch Fetch Threads
-	pthread_t threadFetch_t;
-	pthread_create(&threadFetch_t, NULL, threadFetch, NULL);
+	pthread_t *threadFetchArray = new pthread_t[sizeof(pthread_t) * paramO.numFetch];
 
+	for(int i=0; i<paramO.numFetch; i++)
+	{
+		pthread_create(&threadFetchArray[i], NULL, threadFetch, NULL);
+	}
 
 	// Launch Parse Threads
-	pthread_t threadParse_t;
-	pthread_create(&threadParse_t, NULL, threadParse, &paramO);
-	
+	pthread_t *threadParseArray = new pthread_t[sizeof(pthread_t) * paramO.numParse];
+	for(int i=0; i<paramO.numParse; i++)
+	{
+		pthread_create(&threadParseArray[i], NULL, threadParse, &paramO);
+	}
+
 	// Launch Results printer
 	pthread_t parseResults_t;
 	pthread_create(&parseResults_t, NULL, parseResults, &paramO);
+
+	// Join threads
+	pthread_join(timer_t, NULL);
+	for(int i=0; i<paramO.numParse; i++)
+	{
+		pthread_join(threadParseArray[i], NULL);
+	}
+	for(int i=0; i<paramO.numFetch; i++)
+	{
+		pthread_join(threadFetchArray[i], NULL);
+	}
+	pthread_join(parseResults_t, NULL);
+	
+
+	// Clean up
+	delete threadFetchArray;
+	delete threadParseArray;
 
 	// Close curl	
 	curl_global_cleanup();
 }
 
+void quit_handler(int s)
+{
+	cout << "Quitting..." << endl;
+	// Signal all conds
+	Running = false;
+	pthread_cond_broadcast(&sitesQCond);
+	pthread_cond_broadcast(&dataQCond);
+	pthread_cond_broadcast(&resultsCond);
+	pthread_cond_broadcast(&alarmCond);
+	alarm(0);
+}
+
+void alarmHandler(int sig)
+{
+	pthread_cond_signal(&alarmCond);
+	cout << "TIMER_FUNC" << endl;
+	cout << "Timer Reached 0: Searching all sites..." << endl;
+	pthread_mutex_lock(&sitesQLock);
+	for(string site :paramO.sitesV)
+	{
+		sitesQ.push(site);
+		pthread_cond_signal(&sitesQCond);
+	}
+	pthread_mutex_unlock(&sitesQLock);
+}
 
 void *timer_func(void *arg)
 {
 	params * paramsO = (params *) arg;
+	signal(SIGALRM, alarmHandler);
+	alarmHandler(0);
 	while(Running)
 	{
-		pthread_mutex_lock(&sitesQLock);
-		for(string site :paramsO->sitesV)
-		{
-			sitesQ.push(site);
-			pthread_cond_signal(&sitesQCond);
-		}
-		pthread_mutex_unlock(&sitesQLock);
-		sleep(paramsO->period);
+		pthread_mutex_lock(&alarmLock);
+		alarm(paramsO->period);
+		pthread_cond_wait(&alarmCond, &alarmLock);
+		pthread_mutex_unlock(&alarmLock);
 	}
 }
 
+// Curl websites
 void* threadFetch(void* arg)
 {
 	string item;
-	string data;
-	
+	our_string ret_data;
+
 	while (Running)
 	{
 		struct queueDObject dqo;
 		pthread_mutex_lock(&sitesQLock);	
-		while(sitesQ.empty())
+		while(sitesQ.empty() && Running)
 			pthread_cond_wait(&sitesQCond, &sitesQLock);
+		if(!Running) // We add this extra test to avoid SEG FAULTS when quitting
+		{
+			pthread_mutex_unlock(&sitesQLock);
+			break;
+		}
 		item = sitesQ.front();
 		sitesQ.pop();
 		pthread_mutex_unlock(&sitesQLock);
-		data = our_curl(item);
-		dqo.data = data;
+		
+		ret_data = our_curl(item);
+		if( !ret_data.success)
+		{
+			cout << "\tSkipping " << item << "..." << endl;
+			continue;
+		}
+		dqo.data = ret_data.data;
 		dqo.URL = item;
+		
 		pthread_mutex_lock(&dataQLock);
 		dataQ.push(dqo);
 		pthread_cond_signal(&dataQCond);
@@ -134,15 +206,22 @@ void* threadFetch(void* arg)
 	}
 }
 
+// Search for search term in the website
 void* threadParse(void* arg)
 {
 	params* p = (params*) arg;
-	
+
 	while(Running)
 	{
 		pthread_mutex_lock(&dataQLock);
-		while(dataQ.empty())
+		while(dataQ.empty() && Running)
 			pthread_cond_wait(&dataQCond, &dataQLock);
+		if(!Running)
+		{
+			pthread_mutex_unlock(&dataQLock);
+			break;
+		}
+		
 		queueDObject dqo = dataQ.front();
 		dataQ.pop();
 		pthread_mutex_unlock(&dataQLock);
@@ -155,14 +234,18 @@ void* threadParse(void* arg)
 		for(string term : p->searchesV)
 		{
 			count = 0;
-			pos = dqo.data.find(term);
+			pos = dqo.data.find(term); // returns position of 'term' in 'dqo.data'
 			while(pos != string::npos)
 			{
 				count += 1;
-				current = dqo.data.substr(pos + 1);
+				current = current.substr(pos + 1);
 				pos = current.find(term);
 			}
-			lresult.append(get_time());
+			time_t rawtime;
+			struct tm * myTM;
+			time(&rawtime);
+			myTM = localtime(&rawtime);
+			lresult.append(form_time(myTM));
 			lresult.append(",");
 			lresult.append(term);
 			lresult.append(",");
@@ -170,8 +253,10 @@ void* threadParse(void* arg)
 			lresult.append(",");
 			lresult.append(to_string(count));
 			lresult.append("\n");
-	
+			if(!Running)
+				break;
 		}
+
 
 		// write in results
 		pthread_mutex_lock(&resultsLock);
@@ -183,6 +268,7 @@ void* threadParse(void* arg)
 
 }
 
+// Write results to csv file
 void* parseResults(void *arg)
 {
 	int iteration = 0;
@@ -193,11 +279,11 @@ void* parseResults(void *arg)
 	{
 		iteration += 1;
 		pthread_mutex_lock(&resultsLock);
-		while(nDone < paramO->sitesV.size())
+		while(nDone < paramO->sitesV.size()  && Running)
 		{
 			pthread_cond_wait(&resultsCond, &resultsLock);
 		}
-		// Results should now be in global results string
+		// Results should now be in global string 'results'. Write to csv file
 		string filename = "";
 		filename.append(to_string(iteration));
 		filename.append(".csv");
@@ -209,7 +295,25 @@ void* parseResults(void *arg)
 	}
 }
 
+// Returns string of time
+string form_time(struct tm* t)
+{
+	string s = "";
+	s.append(to_string(t->tm_hour));
+	s.append(":");
+	s.append(to_string(t->tm_min));
+	s.append(":");
+	s.append(to_string(t->tm_sec));
+	s.append(" ");
+	s.append(to_string(t->tm_mon + 1));
+	s.append("/");
+	s.append(to_string(t->tm_mday));
+	s.append("/");
+	s.append(to_string(t->tm_year + 1900));
+	return s;
+}
 
+// Parse configuration text file
 void parse_config(string fileName, params &paramO)
 {
 	string line;
@@ -241,6 +345,11 @@ void parse_config(string fileName, params &paramO)
 						if(test_digit(val))
 						{
 							paramO.numFetch = atoi(val.c_str());
+							if(paramO.numFetch > 8 || paramO.numFetch < 1)
+							{
+								cout << " Incorrent value for NUM_FETCH. Must be between 1 and 8. Setting value to 1" << endl;
+								paramO.numFetch = 1;
+							}
 						}
 						else
 						{
@@ -253,6 +362,11 @@ void parse_config(string fileName, params &paramO)
 						if(test_digit(val))
 						{
 							paramO.numParse = atoi(val.c_str());
+							if(paramO.numParse > 8 || paramO.numParse < 1)
+							{
+								cout << " Incorrent value for NUM_PARSE. Must be between 1 and 8. Setting value to 1" << endl;
+								paramO.numParse = 1;
+							}
 						}
 						else
 						{
@@ -316,6 +430,7 @@ void parse_config(string fileName, params &paramO)
 
 }
 
+// Check if given string is a number
 bool test_digit(string word)
 {
 	if(word.length() < 1)
@@ -330,7 +445,7 @@ bool test_digit(string word)
 	return true;
 }
 
-
+// Part of Curl
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
   size_t realsize = size * nmemb;
@@ -350,12 +465,11 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
   return realsize;
 }
 
- 
-string our_curl(string website)
+ // Curl, but edited to return our_string so we can check for success in threadFetch and cleanup in main
+our_string our_curl(string website)
 {
   CURL *curl_handle;
   CURLcode res;
-  string data;
  
   struct MemoryStruct chunk;
  
@@ -378,7 +492,10 @@ string our_curl(string website)
   /* some servers don't like requests that are made without a user-agent
      field, so we provide one */ 
   curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
- 
+  //curl_easy_setopt(curl_handle, CURLOPT_CONNECTTIMEOUT, 10L);
+
+  //curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, "libcurl-agent/1.0");
+
   /* get it! */ 
   res = curl_easy_perform(curl_handle);
   
@@ -389,7 +506,10 @@ string our_curl(string website)
   if(res != CURLE_OK) {
     cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
     free(chunk.memory);
-    return string("");
+    our_string s;
+    s.data = "";
+    s.success = false;
+    return s;
   }
   else {
     /*
@@ -398,9 +518,11 @@ string our_curl(string website)
      *
      * Do something nice with it!
      */ 
-    data.assign(chunk.memory, chunk.size);
+    our_string s;
+    s.data.assign(chunk.memory, chunk.size);
+    s.success = true;
     free(chunk.memory);
-    return data;
+    return s;
   }
   
  
